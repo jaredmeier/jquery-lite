3de314873d05c1e44741cc371a0ab97a4e55abf1
Jared Meier <meier.jared@gmail.com>
2020-06-23 17:28:26 -0400

    Add ajax request

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ window.$l = (arg) => {
         return new domNode(arg);
     } else if (typeof arg === 'function') {
         if (document.readyState === 'loading') {
+            // if this is the first function added to queue, set up event listener to run whole queue once doc is loaded
             if (!docQueue.length) document.addEventListener('DOMContentLoaded', runDocQueue);
             docQueue.push(arg);
         } else {
@@ -22,3 +23,53 @@ window.$l = (arg) => {
 function runDocQueue() {
     docQueue.forEach(fn => fn());
 }
+
+$l.extend = (merger, ...others) => {
+    others.forEach( other => {
+        for (let prop in other) {
+            merger[prop] = other[prop];
+        }
+    })
+    return merger;
+}
+
+$l.ajax = options => {
+    const req = new XMLHttpRequest();
+    const defaults = {
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        method: 'GET',
+        url: '',
+        success: () => { },
+        error: () => { },
+        data: {},
+    };
+    // set any defaults for options not passed as argument
+    options = $l.extend(defaults, options);
+    options.method = options.method.toUpperCase();
+
+    req.open(options.method, options.url);
+    req.onload = () => {
+        if (req.readyState === 4) {
+            if (req.status === 200) {
+                options.success(JSON.parse(req.response));
+            } else {
+                options.error(JSON.parse(req.response));
+            }
+        }
+    }
+    req.onerror = () => console.log('An error occurred');
+    req.send(JSON.stringify(options.data));
+}
+
+// $.ajax({
+//     type: 'GET',
+//     url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=bcb83c4b54aee8418983c2aff3073b3b",
+//     success(data) {
+//         console.log("We have your weather!")
+//         console.log(data);
+//     },
+//     error() {
+//         console.error("An error occurred.");
+//     },
+// });
+
